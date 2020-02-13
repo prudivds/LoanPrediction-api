8907cdbca8f9f3deb09d0fb397fa32f4547c7bfe
Prudhviraju Srivatsavaya <prudivds@gmail.com>
2020-02-13 13:39:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,85 +7,17 @@ from sklearn.base import BaseEstimator, TransformerMixin
 app = Flask(__name__)
 model = pickle.load(open('DecisionTree.pkl', 'rb'))
 
-class PreProcessing(BaseEstimator, TransformerMixin):
+MinMaxpickle_in = open("minmax_pickle.pkl","rb")
+MinmaxScaler_dict = pickle.load(MinMaxpickle_in)
 
+standardscaler_in = open("standardscaler_pickle.pkl","rb")
+standardscaler_dict = pickle.load(standardscaler_in)
 
-    def __init__(self):
-        pass
+Onehotpickle_in = open("binarizer_pickle.pkl","rb")
+Onehot_dict = pickle.load(Onehotpickle_in)
 
-    
-    ## Here this model will be called with a variable (df), which is a dataframe
-    def datapreprocessing(self, df):
-        
-        df = df.reset_index(drop=True)
-        
-        df.loc[df['Gender'].isnull(),'Gender']='Trasgender'
-        df.loc[df['Dependents'].isnull(),'Dependents']='0'
-        df.loc[df['Education'].isnull(),'Education']='No Education'
-        df.loc[df['Married'].isnull(),'Married']='No'
-        df.loc[df['Self_Employed'].isnull(),'Self_Employed']='No'
-        df.loc[df['Loan_Amount_Term'].isnull(),'Loan_Amount_Term']=0
-        df.loc[df['Credit_History'].isnull(),'Credit_History']=0
-        df.loc[df['LoanAmount'].isnull(),'LoanAmount']=146.867     
-        df['Credit_History']=df['Credit_History'].astype(str)
-        
-        
-        ## Creating a Instance for the minmax Pickle File
-        MinMaxpickle_in = open("minmax_pickle.pkl","rb")
-        MinmaxScaler_dict = pickle.load(MinMaxpickle_in)
-        
-        ## Applying the pickle file
-        df['ApplicantIncome']=pd.DataFrame(MinmaxScaler_dict['ApplicantIncome'].transform(df[['ApplicantIncome']]))
-        
-        ## Creating a Instance for the minmax Pickle File
-        standardscaler_in = open("standardscaler_pickle.pkl","rb")
-        standardscaler_dict = pickle.load(standardscaler_in)
-        
-        ## Applying the pickle file
-        df['LoanAmount']=pd.DataFrame(standardscaler_dict['LoanAmount'].transform(df[['LoanAmount']]))
-        
-        ## Creating a Instance for the LabelEncoder Pickle File
-        Labelencoderpickle_in = open("labelencoder_pickle.pkl","rb")
-        Labelencoder_dict = pickle.load(Labelencoderpickle_in)
-        
-        ## Applying the pickle file
-        df['Education']=pd.DataFrame(Labelencoder_dict['Education'].transform(df[['Education']]))
-        df['Property_Area']=pd.DataFrame(Labelencoder_dict['Property_Area'].transform(df[['Property_Area']]))
-        df['Credit_History']=pd.DataFrame(Labelencoder_dict['Credit_History'].transform(df[['Credit_History']]))
-        df['Dependents']=pd.DataFrame(Labelencoder_dict['Dependents'].transform(df[['Dependents']]))
-        
-        ## Creating a Instance for the binarizer Pickle File
-        Onehotpickle_in = open("binarizer_pickle.pkl","rb")
-        Onehot_dict = pickle.load(Onehotpickle_in)
-        
-        ## Applying the pickle file
-        d1=pd.DataFrame(Onehot_dict['Self_Employed'].transform(df[['Self_Employed']]))
-        d1.columns=['Self_Employed_0']
-        d2=pd.DataFrame(Onehot_dict['Married'].transform(df[['Married']]))
-        d2.columns=['Married_0']
-        d3=pd.DataFrame(Onehot_dict['Gender'].transform(df[['Gender']]))
-        d3.columns=['Gender_0','Gender_1','Gender_2']
-    
-        df=df.drop(['Self_Employed','Married','Gender'],axis=1)
-        df=pd.concat([df,d1,d2,d3],axis=1)
-        
-        return df.as_matrix()
-
-    def definingvalues(self, df, y=None,**fit_params):
-        self.term_mean_ = df['Loan_Amount_Term'].mean()
-        self.amt_mean_ = df['LoanAmount'].mean()
-        return self
-    
-    def encodingTargetVariable(self,df):
-        Labelencoderpickle_in = open("labelencoder_pickle.pkl","rb")
-        Labelencoder_dict = pickle.load(Labelencoderpickle_in)
-        
-        ## Applying the pickle file
-        df['Loan_Status']=pd.DataFrame(Labelencoder_dict['Loan_Status'].transform(df[['Loan_Status']]))
-        
-        return df.as_matrix()
-
-preprocess = PreProcessing()
+Labelencoderpickle_in = open("labelencoder_pickle.pkl","rb")
+Labelencoder_dict = pickle.load(Labelencoderpickle_in)
 
 @app.route('/')
 def home():
@@ -102,19 +34,30 @@ def predict():
        'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
        'Loan_Amount_Term', 'Credit_History', 'Property_Area'])
     
-    data_transformed_xtrain = preprocess.datapreprocessing(df)
+    df['Credit_History']=df['Credit_History'].astype(str)
     
-    pred_var = ['Dependents', 'Education', 'ApplicantIncome',
-       'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History',
-       'Property_Area', 'Self_Employed_0', 'Married_0', 'Gender_0', 'Gender_1','Gender_2']
-
-
-    data_transformed_xtrain=pd.DataFrame(data_transformed_xtrain,columns=pred_var)
+    d1=pd.DataFrame(Onehot_dict['Self_Employed'].transform(df[['Self_Employed']]))
+    d1.columns=['Self_Employed_0']
+    d2=pd.DataFrame(Onehot_dict['Married'].transform(df[['Married']]))
+    d2.columns=['Married_0']
+    d3=pd.DataFrame(Onehot_dict['Gender'].transform(df[['Gender']]))
+    d3.columns=['Gender_0','Gender_1','Gender_2']
     
-    prediction = model.predict(data_transformed_xtrain)
+    df['Dependents']=pd.DataFrame(Labelencoder_dict['Dependents'].transform(df[['Dependents']]))
+    df['Education']=pd.DataFrame(Labelencoder_dict['Education'].transform(df[['Education']]))
+    df['Property_Area']=pd.DataFrame(Labelencoder_dict['Property_Area'].transform(df[['Property_Area']]))
+    df['Credit_History']=pd.DataFrame(Labelencoder_dict['Credit_History'].transform(df[['Credit_History']]))
+    
+    df['LoanAmount']=pd.DataFrame(standardscaler_dict['LoanAmount'].transform(df[['LoanAmount']]))
+    df['ApplicantIncome']=pd.DataFrame(MinmaxScaler_dict['ApplicantIncome'].transform(df[['ApplicantIncome']]))
+    
+    df=pd.concat([df,d1,d2,d3],axis=1)
+    
+    df=df.drop(['Self_Employed','Married','Gender'],axis=1)
+    prediction = model.predict(df)
 
     output = round(prediction[0], 2)
-    
+
     if output==1:
         output='Loan Approved'
     else:
